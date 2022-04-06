9c3e28a760c18bda0244db73c73936e809272733
xyz2b <jiaohongyua@gmail.com>
2022-04-06 17:46:25 +0800

    openops project init

---
+++ openops-server/src/test/java/com/openops/openopsserver/OpenopsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openops.openopsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenopsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
