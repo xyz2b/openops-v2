0f10d14b272859b7931cb331fc2010c3e8e0a98d
xyz2b <jiaohongyua@gmail.com>
2022-04-11 18:07:37 +0800

    server process

--- openops-server/src/main/java/com/openops/process/CommandExecuteRequestServerProcessor.java
+++ openops-server/src/main/java/com/openops/process/CommandExecuteRequestServerProcessor.java
@@ -3,6 +3,7 @@ package com.openops.process;
 import com.openops.common.process.AbstractProcessor;
 import com.openops.common.session.Session;
 
+// 接收apiserver请求的处理器
 public class CommandExecuteRequestServerProcessor extends AbstractProcessor {
     public CommandExecuteRequestServerProcessor(int type) {
         super(type);
