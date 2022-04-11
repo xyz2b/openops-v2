3ecf767ea0825b16e3c3956dff7b9eee65e259ce
xyz2b <jiaohongyua@gmail.com>
2022-04-11 18:09:26 +0800

    server process commit

--- openops-server/src/main/java/com/openops/process/CommandExecuteRequestServerProcessor.java
+++ openops-server/src/main/java/com/openops/process/CommandExecuteRequestServerProcessor.java
@@ -4,6 +4,7 @@ import com.openops.common.process.AbstractProcessor;
 import com.openops.common.session.Session;
 
 // 接收apiserver请求的处理器
+// apiserver的请求，client_id为0.0.0.0，通过client_id判断是apiserver发送的请求还是真实client发送的请求
 public class CommandExecuteRequestServerProcessor extends AbstractProcessor {
     public CommandExecuteRequestServerProcessor(int type) {
         super(type);
